08ae624c11c0408f52b6733f37d05433433fa642
unknown <gaurangmishra2712@gmail.com>
2023-12-03 01:02:11 +0530

    react-basic

---
+++ App.js
@@ -0,0 +1,9 @@
+const he=React.createElement("div", {id: "parent"}, 
+[React.createElement("div", {id: "child"}, 
+[React.createElement("h1", {}, "I'm a h1 tag"),
+React.createElement("h2", {}, "I'm a h2 tag")]),
+React.createElement("div", {id: "child2"}, 
+[React.createElement("h1", {}, "I'm a h1 tag"),
+React.createElement("h2", {}, "I'm a h2 tag")])]);
+const re=ReactDOM.createRoot(document.getElementById("root"));
+re.render(he);
