02ef0b3f8edf396de65a3caabd813eccd44616f1
Gaurang Mishra <gaurangmishra2712@gmail.com>
2023-12-10 17:00:06 +0530

    JSX

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
+// JSX Functional Component
+const JsxHeading = () => (
+  <h1 className="head" tabIndex={5}>
+    Namaste Javascript using JSX
+  </h1>
+);
+// JSX element
+const jsxHeading = (
+  <h1 className="head" tabIndex={5}>
+    Namaste Javascript using JSX
+  </h1>
+);
+// Component Composition -  Component inside a component
+//JSX element inside a component
+// {anything inside this in javascript inside JSX}
+const Heading = () => (
+  <div id="container">
+    {jsxHeading}
+    {JsxHeading()}
+    <JsxHeading />
+    <h1>Namaste Javascript using Functional Component</h1>
+  </div>
+);
 
-const he=React.createElement("div", {id: "parent"}, 
-[React.createElement("div", {id: "child"}, 
-[React.createElement("h1", {}, "Namaste React"),
-React.createElement("h2", {}, "I'm a h2 tag")]),
-React.createElement("div", {id: "child2"}, 
-[React.createElement("h1", {}, "I'm a h1 tag"),
-React.createElement("h2", {}, "I'm a h2 tag")])]);
-const re=ReactDOM.createRoot(document.getElementById("root"));
-re.render(he);
+//console.log(jsxheading);
+const re = ReactDOM.createRoot(document.getElementById("root"));
+re.render(<Heading />);
