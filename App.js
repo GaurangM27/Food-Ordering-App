3d8666b724112efd1fac0632a1598b63ef7a3467
Gaurang Mishra <gaurangmishra2712@gmail.com>
2023-12-10 16:58:32 +0530

    JSX

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 const he=React.createElement("div", {id: "parent"}, 
 [React.createElement("div", {id: "child"}, 
-[React.createElement("h1", {}, "I'm a h1 tag"),
+[React.createElement("h1", {}, "Namaste React"),
 React.createElement("h2", {}, "I'm a h2 tag")]),
 React.createElement("div", {id: "child2"}, 
 [React.createElement("h1", {}, "I'm a h1 tag"),
